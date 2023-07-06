fe4514715cd96352ea35a9668b5aa40854ca04be
ehddnr <ehddnrdlaos@gmail.com>
2023-07-06 23:20:22 +0900

    초기 커밋.

---
+++ step1/No_1.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+
+
+//1.
+//int main()
+//{
+//	std::cout << "Hello World!" << std::endl;
+//}
+
+
+
+//2.
+//int main()
+//{
+//	int A{}, B{};
+//	std::cin >> A >> B;
+//
+//	std::cout << A + B << std::endl;
+//}
+
+//3.
+//int main()
+//{
+//	int A{}, B{};
+//	std::cin >> A >> B;
+//
+//	std::cout << A - B << std::endl;
+//}
+
+//4.
+//int main()
+//{
+//	int A{}, B{};
+//	std::cin >> A >> B;
+//
+//	std::cout << A * B << std::endl;
+//}
+
+//5.
+//int main()
+//{
+//	double A{}, B{};
+//	std::cin >> A >> B;
+//		
+//	std::cout << std::fixed;
+//	std::cout.precision(9);
+//
+//	std::cout << A / B << std::endl;
+//}
+
+//6.
+//int main()
+//{
+//	int A{}, B{};
+//	std::cin >> A >> B;
+//
+//	std::cout << A + B << std::endl;
+//	std::cout << A - B << std::endl;
+//	std::cout << A * B << std::endl;
+//	std::cout << A / B << std::endl;
+//	std::cout << A % B << std::endl;
+//}
+
+//7.
+//int main()
+//{
+//	const int x{ 51 };
+//	char name[x]{};
+//	std::cin >> name;	
+//		
+//	std::cout << name << "??!" << std::endl;
+//	
+//}
+
+//8.
+//int main()
+//{
+//	int x{};
+//	std::cin >> x;
+//	
+//	std::cout << x - 543 << std::endl;
+//}
+
+//9.
+//int main()
+//{
+//	int A{}, B{}, C{};
+//	std::cin >> A >> B >> C;
+//
+//	std::cout << (A + B) % C << std::endl;
+//	std::cout << ((A % C) + (B % C))%C << std::endl;
+//	std::cout << (A * B) % C << std::endl;
+//	std::cout << ((A % C) * (B % C)) % C << std::endl;	
+//}
+
+//10.
+//int main()
+//{
+//	int A{}, B{}, C{}, D{}, E{};
+//	std::cin >> A >> B;
+//	
+//	E = B / 100;
+//	D = B / 10 - 10*E;
+//	C = B - 100 * E - 10 * D;
+//
+//	std::cout << A * C << std::endl;
+//	std::cout << A * D << std::endl;
+//	std::cout << A * E << std::endl;
+//	std::cout << A * B << std::endl;
+//}
+
+//11.
+//int main()
+//{
+//	long long A{}, B{}, C{};
+//	std::cin >> A >> B >> C;
+//
+//	std::cout << A + B + C << std::endl;
+//}
+
+//12.
+//int main()
+//{
+//	std::cout << "\\    /\\" << std::endl;
+//	std::cout << " )  ( ')" << std::endl;
+//	std::cout << "(  /  )" << std::endl;
+//	std::cout << " \\(__)|" << std::endl;
+//}
+
+//13.
+//int main()
+//{
+//	std::cout << "|\\_/|" << std::endl;
+//	std::cout << "|q p|   /}" << std::endl;
+//	std::cout << "( 0 )\"\"\"\\" << std::endl;
+//	std::cout << "|\"^\"`    |" << std::endl;
+//	std::cout << "||_/=\\\\__|" << std::endl;
+//}
