ba69e55ec26177a4b1b2a86974145ee5790ea4a1
ehddnr <ehddnrdlaos@gmail.com>
2023-07-14 21:36:16 +0900

    step3 1~5

---
+++ step3/No_3.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+//1.
+//int main()
+//{
+//	int N{}, a{};
+//	std::cin >> N;
+//
+//	for (int i = 1; i <= 9;i++)
+//	{
+//		a = N * i;
+//		std::cout << N << " * " << i << " = " << a << std::endl;
+//	}
+//}
+
+//2.
+//int main()
+//{
+//	int A{}, B{}, T{};
+//	std::cin >> T;
+//
+//	for (int i = 0; i < T;i++)
+//	{
+//		std::cin >> A >> B;
+//		std::cout << A + B << std::endl;
+//	}
+//}
+
+//3.
+//int main()
+//{
+//	int a{}, n{};
+//	std::cin >> n;
+//
+//	for (int i = 0; i < n;i++)
+//	{
+//		a = a + i + 1;
+//	}
+//
+//	std::cout << a << std::endl;
+//}
+
+//4.
+//int main()
+//{
+//	int X{}, N{}, a{}, b{}, C{};
+//
+//	std::cin >> X; 
+//	std::cin >> N;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		std::cin >> a >> b;
+//		C = a * b + C;
+//	}
+//	if (C == X)
+//	{
+//		std::cout << "Yes" << std::endl;
+//	}
+//	else
+//	{
+//		std::cout << "No" << std::endl;
+//	}
+//
+//}
+
+
+//5.
+//int main()
+//{
+//	int N{};
+//	std::cin >> N;
+//
+//	N = N / 4;
+//	for (int i = 0; i < N; i++)
+//	{
+//		std::cout << "long ";
+//	}std::cout << "int" << std::endl;
+//}
