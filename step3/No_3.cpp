95f29057f3b1cb03ce1a01c4eaca0680efeed07e
ehddnr <ehddnrdlaos@gmail.com>
2023-07-27 23:38:02 +0900

    Update/0727 6~11

--- step3/No_3.cpp
+++ step3/No_3.cpp
@@ -77,3 +77,99 @@
 //		std::cout << "long ";
 //	}std::cout << "int" << std::endl;
 //}
+
+//6.
+//using namespace std;
+//
+//int main()
+//{
+//	cin.tie(NULL);
+//	ios_base::sync_with_stdio(false);
+//	
+//	int A{}, B{}, T{};
+//
+//	cin >> T;
+//
+//	for (int i = 0; i < T; i++)
+//	{
+//		cin >> A >> B;
+//
+//		cout << A + B << "\n";
+//	}
+//}
+
+//7.
+//int main()
+//{	
+//	int A{}, B{}, T{};
+//
+//	std::cin >> T;
+//
+//	for (int i = 0; i < T; i++)
+//	{
+//		std::cin >> A >> B;
+//
+//		std::cout << "Case #" << i+1 <<": " << A + B << std::endl;
+//	}
+//}
+
+
+//8.
+//int main()
+//{	
+//	int A{}, B{}, T{};
+//
+//	std::cin >> T;
+//
+//	for (int i = 0; i < T; i++)
+//	{
+//		std::cin >> A >> B;
+//
+//		std::cout << "Case #" << i+1 <<": " << A << " + " << B << " = " << A + B << std::endl;
+//	}
+//}
+
+//9.
+//int main()
+//{
+//	int N{};
+//	std::cin >> N;
+//	for (int i = 1; i < N+1; i++)
+//	{
+//		for (int j = 1; j <= i;j++)
+//		{
+//			std::cout << "*";
+//		} std::cout << std::endl;
+//	}
+//}
+
+//10.
+//int main()
+//{
+//	int N{};
+//	std::cin >> N;
+//	for (int i = 1; i < N + 1; i++)
+//	{
+//		for(int z = 1; z<= N-i;z++)
+//		{
+//			std::cout << " ";
+//		}
+//		for (int j = 1; j <= i;j++)
+//		{
+//			std::cout << "*";
+//		} std::cout << std::endl;
+//	}
+//}
+
+//11.
+//int main()
+//{	
+//	int A{}, B{};
+//
+//	for (; ;)
+//	{
+//		std::cin >> A >> B;
+//		if (std::cin.eof()) break;
+//		std::cout << A + B << std::endl;		
+//	}
+//}
