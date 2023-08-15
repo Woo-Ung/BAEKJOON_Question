35f425053252a2cf137088804acf98e78d6745dc
ehddnr <ehddnrdlaos@gmail.com>
2023-08-15 23:21:56 +0900

    Update/0815 1

---
+++ step5/No_5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//1.
+//int main()
+//{
+//	int x{};
+//	std::string word;
+//
+//	std::cin >> word;
+//	std::cin >> x;
+//
+//	x -= 1;
+//
+//	std::cout << word[x] << std::endl;
+//}
