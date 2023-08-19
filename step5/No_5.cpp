1d64df04edecf850e262ef422c87e1880c8747ff
ehddnr <ehddnrdlaos@gmail.com>
2023-08-19 23:18:12 +0900

    Update/0819 2~3

--- step5/No_5.cpp
+++ step5/No_5.cpp
@@ -13,3 +13,46 @@
 //
 //	std::cout << word[x] << std::endl;
 //}
+
+//2.
+//int main()
+//{
+//	int x{};
+//	std::string word;
+//
+//	std::cin >> word;
+//
+//	for (int e : word)
+//	{
+//		if (e == NULL)
+//			break;
+//		x++;
+//	}
+//
+//	std::cout << x << std::endl;
+//}
+
+//3.
+//int main()
+//{
+//	int  T{};
+//	std::string word;
+//
+//	std::cin >> T;
+//
+//	for (int i = 0;i < T;i++)
+//	{		
+//		std::cin >> word;
+//
+//		int count{};
+//
+//		for (int e : word)
+//		{						
+//			if (e == NULL)
+//				break;
+//
+//			count++;
+//		}		
+//		std::cout << word[0] << word[count-1] << std::endl;		
+//	}	
+//}
