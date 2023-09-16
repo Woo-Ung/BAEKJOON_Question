9c3569e10340cb55772ff3c025f35df818de3012
ehddnr <ehddnrdlaos@gmail.com>
2023-09-16 23:59:42 +0900

    Update/0916 3

--- step6/No_6.cpp
+++ step6/No_6.cpp
@@ -15,19 +15,53 @@
 //}
 
 //2.
+//int main()
+//{
+//	int a[6]{ 1,1,2,2,2,8 };
+//	int b[6]{};
+//
+//	for (int i = 0; i < 6;i++)
+//	{
+//		std::cin >> b[i];
+//	}
+//
+//	for (int i = 0; i < 6;i++)
+//	{
+//		std::cout << a[i] - b[i] << " ";
+//	} std::cout << std::endl;
+//
+//}
+
+//3.
 int main()
 {
-	int a[6]{ 1,1,2,2,2,8 };
-	int b[6]{};
+	int N{};
+	std::cin >> N;
 
-	for (int i = 0; i < 6;i++)
+	for (int i = 0; i < (2 * N) - 1; i++)
 	{
-		std::cin >> b[i];
+		if (i < N)
+		{
+			for (int j = N; j > i + 1; j--)
+			{
+				std::cout << " ";
+			}
+			for (int j = 0; j < (2 * i) + 1; j++)
+			{
+				std::cout << "*";
+			}
+		}
+		else
+		{
+			for (int j = N; j <= i; j++)
+			{
+				std::cout << " ";
+			}
+			for (int j = 2 * (2 * N - i) - 3; j > 0; j--)
+			{
+				std::cout << "*";
+			}
+		}
+		std::cout << std::endl;
 	}
-
-	for (int i = 0; i < 6;i++)
-	{
-		std::cout << a[i] - b[i] << " ";
-	} std::cout << std::endl;
-
 }
