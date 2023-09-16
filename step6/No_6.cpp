3dbce412f23dc1c63fcde01d103cffd60626d33b
ehddnr <ehddnrdlaos@gmail.com>
2023-09-17 00:32:17 +0900

    Update/0917 3~4

--- step6/No_6.cpp
+++ step6/No_6.cpp
@@ -33,35 +33,63 @@
 //}
 
 //3.
-int main()
-{
-	int N{};
-	std::cin >> N;
+//int main()
+//{
+//	int N{};
+//	std::cin >> N;
+//
+//	for (int i = 0; i < 2 * N - 1; i++)
+//	{
+//		if (i < N)
+//		{
+//			for (int j = N; j > i + 1; j--)
+//			{
+//				std::cout << " ";
+//			}
+//			for (int j = 0; j < 2 * i + 1; j++)
+//			{
+//				std::cout << "*";
+//			}
+//		}
+//		if (i >= N)
+//		{
+//			for (int j = N; j <= i; j++)
+//			{
+//				std::cout << " ";
+//			}
+//			for (int j = (2 * N - 1) - 2 * (i - N+1); j > 0; j--)
+//			{
+//				std::cout << "*";
+//			}			
+//		}std::cout << std::endl;
+//	}
+//}
 
-	for (int i = 0; i < (2 * N) - 1; i++)
-	{
-		if (i < N)
-		{
-			for (int j = N; j > i + 1; j--)
-			{
-				std::cout << " ";
-			}
-			for (int j = 0; j < (2 * i) + 1; j++)
-			{
-				std::cout << "*";
-			}
-		}
-		else
-		{
-			for (int j = N; j <= i; j++)
-			{
-				std::cout << " ";
-			}
-			for (int j = 2 * (2 * N - i) - 3; j > 0; j--)
-			{
-				std::cout << "*";
-			}
-		}
-		std::cout << std::endl;
-	}
-}
+//4.
+//int main()
+//{
+//	int count{};
+//	std::string word;
+//	std::cin >> word;
+//
+//	if(size(word) / 2 == 0)
+//	{
+//		std::cout << 1 << std::endl;
+//	}
+//	for (int i = 0; i < size(word)/2; i++)
+//	{
+//		if (word[i] == word[size(word) - i - 1])
+//		{
+//			count++;
+//		}
+//		else
+//		{
+//			std::cout << 0 << std::endl;
+//			break;
+//		}
+//		if (count == size(word)/2)
+//		{
+//			std::cout << 1 << std::endl;
+//		}
+//	}
+//}
