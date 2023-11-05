e242bf73832df85c5eef319dca6455f876598e1b
ehddnr <ehddnrdlaos@gmail.com>
2023-11-05 23:19:09 +0900

    Update/1105 3

--- step8/no.8.cpp
+++ step8/no.8.cpp
@@ -30,76 +30,98 @@
 //	std::cout << sum << std::endl;
 //}
 
-//2.
-char convert(int a)
-{	
-	
-	if (a>9)
-	{
-		a += 55;
-	}
-	else
-	{
-		a += 48;
-	}
+////2.
+//char convert(int a)
+//{	
+//	
+//	if (a>9)
+//	{
+//		a += 55;
+//	}
+//	else
+//	{
+//		a += 48;
+//	}
+//
+//	return (char)a;
+//}
+//
+//int main()
+//{
+//	int Number{}, share{}, div{};
+//
+//	std::string x{};
+//
+//	std::cin >> Number >> share;
+//
+//	div = Number;
+//
+//	for(int i = 0; ;i++)
+//	{
+//		if (div < share)
+//		{			
+//			x += convert(div);
+//			
+//			break;
+//		}
+//
+//		if (i == 0)
+//		{
+//			if (div % share == 0)
+//			{
+//				if (div / share >= share)
+//				{
+//					x += '0';
+//				}
+//				else
+//				{
+//					x += convert(div / share);
+//				}
+//				if (div / share < share)
+//				{
+//					if (share > 1)
+//					{
+//						x += '0';
+//						reverse(x.begin(), x.end());
+//					}
+//					break;
+//				}
+//			}
+//			else
+//			{
+//				x += convert(div % share);
+//			}
+//		}
+//		else
+//		{
+//			x += convert(div % share);
+//		}
+//		div = div / share;		
+//	}
+//
+//	reverse(x.begin(), x.end());
+//
+//	std::cout << x << std::endl;
+//}
 
-	return (char)a;
-}
-
+//3.
 int main()
 {
-	int Number{}, share{}, div{};
+	int T{}, C{};
+	int array[4]{25,10,5,1};
+	int x[4]{};
 
-	std::string x{};
+	std::cin >> T;
 
-	std::cin >> Number >> share;
-
-	div = Number;
-
-	for(int i = 0; ;i++)
+	for (int i = 0;i < T;i++)
 	{
-		if (div < share)
-		{			
-			x += convert(div);
-			
-			break;
-		}
+		std::cin >> C;		
 
-		if (i == 0)
+		for (int j = 0;j < 4;j++)
 		{
-			if (div % share == 0)
-			{
-				if (div / share >= share)
-				{
-					x += '0';
-				}
-				else
-				{
-					x += convert(div / share);
-				}
-				if (div / share < share)
-				{
-					if (share > 1)
-					{
-						x += '0';
-						reverse(x.begin(), x.end());
-					}
-					break;
-				}
-			}
-			else
-			{
-				x += convert(div % share);
-			}
-		}
-		else
-		{
-			x += convert(div % share);
-		}
-		div = div / share;		
+			x[j] = C / array[j];
+			C = C % array[j];
+			std::cout << x[j] << " ";
+		}std::cout << std::endl;		
 	}
-
-	reverse(x.begin(), x.end());
-
-	std::cout << x << std::endl;
 }
