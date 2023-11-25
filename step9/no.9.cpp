25e67a605a4f74625ebdcf42975bbbcd18c69c7a
ehddnr <ehddnrdlaos@gmail.com>
2023-11-25 23:32:12 +0900

    Update/1125 2

--- step9/no.9.cpp
+++ step9/no.9.cpp
@@ -1,32 +1,58 @@
 #include <iostream>
 
 //1.
+//int main()
+//{
+//	int first{}, second{};
+//
+//	while (true)
+//	{
+//		std::cin >> first >> second;
+//		if (first == 0 && second == 0)
+//		{
+//			break;
+//		}
+//
+//		if (second % first == 0)
+//		{
+//			std::cout << "factor" << std::endl;
+//		}
+//
+//		else if (first % second == 0)
+//		{
+//			std::cout << "multiple" << std::endl;
+//		}
+//
+//		else
+//		{
+//			std::cout << "neither" << std::endl;
+//		}
+//	}
+//
+//}
+
+//2.
 int main()
 {
-	int first{}, second{};
+	int N{}, K{}, factors{}, count{};
 
-	while (true)
+	std::cin >> N >> K;
+
+	for (int i = 1; i <= N; i++)
 	{
-		std::cin >> first >> second;
-		if (first == 0 && second == 0)
+		if (N % i == 0)
 		{
-			break;
-		}
-
-		if (second % first == 0)
-		{
-			std::cout << "factor" << std::endl;
-		}
-
-		else if (first % second == 0)
-		{
-			std::cout << "multiple" << std::endl;
-		}
-
-		else
-		{
-			std::cout << "neither" << std::endl;
+			factors = i;
+			count++;
+			if (K == count)
+			{
+				std::cout << factors << std::endl;
+				break;
+			}			
 		}
 	}
-
+	if (K > count)
+	{
+		std::cout << "0" << std::endl;
+	}
 }
