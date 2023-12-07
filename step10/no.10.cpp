6c7d6da34c26e158748eb4dd9411641fe3367229
ehddnr <ehddnrdlaos@gmail.com>
2023-12-07 23:39:15 +0900

    Update/1207 2

--- step10/no.10.cpp
+++ step10/no.10.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
+#include <cstdlib>
 
 //1.
+//int main()
+//{
+//	int A{}, B{};
+//
+//	std::cin >> A >> B;
+//
+//	std::cout << A * B << std::endl;
+//}
+
+//2.
 int main()
 {
-	int A{}, B{};
+	int o[2]{ 0,0 }, me[2]{}, x[2]{};
+	int min{1001};
 
-	std::cin >> A >> B;
+	std::cin >> me[0] >> me[1] >> x[0] >> x[1];
 
-	std::cout << A * B << std::endl;
+	for (int i = 0;i < 2;i++)
+	{		
+		if (min > abs(me[i] - x[i]))
+		{
+			min = abs(me[i] - x[i]);
+		}
+		if (min > abs(me[i] - o[i]))
+		{
+			min = abs(me[i] - o[i]);
+		}		
+	}
+	
+	std::cout << min << std::endl;
 }
