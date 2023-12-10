530df856bb3f15780b382bddbf484662fd6ddcbf
ehddnr <ehddnrdlaos@gmail.com>
2023-12-10 22:31:41 +0900

    Update/1210 3

--- step10/no.10.cpp
+++ step10/no.10.cpp
@@ -12,24 +12,71 @@
 //}
 
 //2.
+//int main()
+//{
+//	int o[2]{ 0,0 }, me[2]{}, x[2]{};
+//	int min{1001};
+//
+//	std::cin >> me[0] >> me[1] >> x[0] >> x[1];
+//
+//	for (int i = 0;i < 2;i++)
+//	{		
+//		if (min > abs(me[i] - x[i]))
+//		{
+//			min = abs(me[i] - x[i]);
+//		}
+//		if (min > abs(me[i] - o[i]))
+//		{
+//			min = abs(me[i] - o[i]);
+//		}		
+//	}
+//	
+//	std::cout << min << std::endl;
+//}
+
+//3.
 int main()
 {
-	int o[2]{ 0,0 }, me[2]{}, x[2]{};
-	int min{1001};
+	int x[3]{}, y[3]{};
+	int a{}, b{};
 
-	std::cin >> me[0] >> me[1] >> x[0] >> x[1];
+	for (int i = 0; i < 3;i++)
+	{
+		std::cin >> x[i] >> y[i];
+		
+	}
 
-	for (int i = 0;i < 2;i++)
+	for (int i = 0; i < 1;i++)
 	{		
-		if (min > abs(me[i] - x[i]))
+		if (x[i] == x[i + 1])
 		{
-			min = abs(me[i] - x[i]);
+			a = x[i + 2];
 		}
-		if (min > abs(me[i] - o[i]))
+		else if (x[i] == x[i + 2])
 		{
-			min = abs(me[i] - o[i]);
+			a = x[i + 1];
+		}
+		else
+		{
+			a = x[i];
 		}		
 	}
-	
-	std::cout << min << std::endl;
+
+	for (int i = 0; i < 1;i++)
+	{
+		if (y[i] == y[i + 1])
+		{
+			b = y[i + 2];
+		}
+		else if (y[i] == y[i + 2])
+		{
+			b = y[i + 1];
+		}
+		else
+		{
+			b = y[i];
+		}
+	}
+
+	std::cout << a << " " << b << std::endl;
 }
