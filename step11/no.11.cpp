ac2bbe6f6c60df24c20ee897223106cbdfd30c83
ehddnr <ehddnrdlaos@gmail.com>
2023-12-24 23:26:08 +0900

    Update/1224 2

--- step11/no.11.cpp
+++ step11/no.11.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
 //1.
+//int main()
+//{
+//	int n{};
+//	std::cin >> n;
+//	std::cout << 1 << std::endl;
+//	std::cout << 0 << std::endl;
+//}
+
+//2.
 int main()
 {
 	int n{};
 	std::cin >> n;
+	std::cout << n << std::endl;
 	std::cout << 1 << std::endl;
-	std::cout << 0 << std::endl;
 }
