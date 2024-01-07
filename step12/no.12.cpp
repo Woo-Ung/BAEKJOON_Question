6626753eaec54a0ca64683f0788922fd614c3e5f
ehddnr <ehddnrdlaos@gmail.com>
2024-01-08 00:28:46 +0900

    Update/0108 2

--- step12/no.12.cpp
+++ step12/no.12.cpp
@@ -1,46 +1,78 @@
 #include <iostream>
 
 //1.
+//int main()
+//{
+//	int N{}, M{}, temp{}, max{};
+//
+//	std::cin >> N >> M;
+//
+//	int* card = new int[N];
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		std::cin >> card[i];	
+//	}
+//
+//	for (int i = 0; i < N;i++)
+//	{
+//		for (int j = i+1 ;j < N;j++)
+//		{
+//			if (card[i] < card[j])
+//			{
+//				temp = card[i];
+//				card[i] = card[j];
+//				card[j] = temp;
+//			}
+//		}
+//	}
+//
+//	for (int i = 0;i < N;i++)
+//	{
+//		for (int j = i + 1; j < N;j++)
+//		{
+//			for (int k = j + 1; k < N;k++)
+//			{
+//				if (max < card[i] + card[j] + card[k] && card[i] + card[j] + card[k] <= M)
+//				{
+//					max = card[i] + card[j] + card[k];
+//				}
+//			}
+//		}
+//	}
+//	std::cout << max << std::endl;
+//
+//	delete[] card;
+//}
+
+//2.
 int main()
 {
-	int N{}, M{}, temp{}, max{};
+	int N{}, sum{}, temp{};
 
-	std::cin >> N >> M;
+	std::cin >> N;
 
-	int* card = new int[N];
-
-	for (int i = 0; i < N; i++)
+	for (int i = 0; ;i++)
 	{
-		std::cin >> card[i];	
-	}
+		temp = i;
+		sum = 0;
 
-	for (int i = 0; i < N;i++)
-	{
-		for (int j = i+1 ;j < N;j++)
+		while (temp != 0)
+		{	
+			sum += temp % 10;
+
+			temp /= 10;
+		}
+
+		if (N == i + sum)
+		{			
+			std::cout << i << std::endl;
+			break;
+		}
+		if (N < i)
 		{
-			if (card[i] < card[j])
-			{
-				temp = card[i];
-				card[i] = card[j];
-				card[j] = temp;
-			}
+			std::cout << 0 << std::endl;
+			break;
 		}
 	}
-
-	for (int i = 0;i < N;i++)
-	{
-		for (int j = i + 1; j < N;j++)
-		{
-			for (int k = j + 1; k < N;k++)
-			{
-				if (max < card[i] + card[j] + card[k] && card[i] + card[j] + card[k] <= M)
-				{
-					max = card[i] + card[j] + card[k];
-				}
-			}
-		}
-	}
-	std::cout << max << std::endl;
-
-	delete[] card;
 }
