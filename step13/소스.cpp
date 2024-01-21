1c097e085d1595878cc807da9a7366a269c6c43e
ehddnr <ehddnrdlaos@gmail.com>
2024-01-21 22:20:47 +0900

    Update/0121 2

--- step13/소스.cpp
+++ step13/소스.cpp
@@ -1,21 +1,54 @@
 #include <iostream>
 
 //1.
+//int main()
+//{
+//	int N{};
+//
+//	std::cin >> N;
+//
+//	int* num = new int[N];
+//
+//	for (int i = 0; i < N;i++)
+//	{
+//		std::cin >> num[i];
+//	}
+//	for (int i = 0; i < N;i++)
+//	{
+//		for (int j = i + 1; j < N;j++)
+//		{
+//			if (num[i] > num[j])
+//			{
+//				int temp = num[i];
+//				num[i] = num[j];
+//				num[j] = temp;
+//			}
+//		}
+//	}
+//
+//	for (int i = 0; i < N;i++)
+//	{
+//		std::cout << num[i] << std::endl;
+//	}
+//
+//	delete[] num;
+//}
+
+//2.
 int main()
 {
-	int N{};
+	int num[5]{};
+	int sum{};
 
-	std::cin >> N;
-
-	int* num = new int[N];
-
-	for (int i = 0; i < N;i++)
+	for (int i = 0; i < 5;i++)
 	{
 		std::cin >> num[i];
+		sum += num[i];
 	}
-	for (int i = 0; i < N;i++)
+
+	for (int i = 0; i < 5;i++)
 	{
-		for (int j = i + 1; j < N;j++)
+		for (int j = i + 1; j < 5;j++)
 		{
 			if (num[i] > num[j])
 			{
@@ -26,10 +59,7 @@ int main()
 		}
 	}
 
-	for (int i = 0; i < N;i++)
-	{
-		std::cout << num[i] << std::endl;
-	}
+	std::cout << sum/5 << std::endl;
+	std::cout << num[2] << std::endl;
 
-	delete[] num;
 }
