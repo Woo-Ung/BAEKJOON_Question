58f5bd6ab70eaf3bd7840cec75532ed4129a31e8
ehddnr <ehddnrdlaos@gmail.com>
2024-01-22 23:48:58 +0900

    Update/0122 3

--- step13/no.13.cpp
+++ step13/no.13.cpp
@@ -35,30 +35,63 @@
 //}
 
 //2.
+//int main()
+//{
+//	int num[5]{};
+//	int sum{};
+//
+//	for (int i = 0; i < 5;i++)
+//	{
+//		std::cin >> num[i];
+//		sum += num[i];
+//	}
+//
+//	for (int i = 0; i < 5;i++)
+//	{
+//		for (int j = i + 1; j < 5;j++)
+//		{
+//			if (num[i] > num[j])
+//			{
+//				int temp = num[i];
+//				num[i] = num[j];
+//				num[j] = temp;
+//			}
+//		}
+//	}
+//
+//	std::cout << sum/5 << std::endl;
+//	std::cout << num[2] << std::endl;
+//}
+
+//3.
 int main()
 {
-	int num[5]{};
-	int sum{};
+	int N{}, k{}, x{};
 
-	for (int i = 0; i < 5;i++)
+	std::cin >> N >> k;
+
+	int* student = new int[N];
+
+	for (int i = 0; i < N;i++)
 	{
-		std::cin >> num[i];
-		sum += num[i];
+		std::cin >> x;
+		student[i] = x;
 	}
 
-	for (int i = 0; i < 5;i++)
+	for (int i = 0; i < N;i++)
 	{
-		for (int j = i + 1; j < 5;j++)
+		for (int j = i + 1; j < N;j++)
 		{
-			if (num[i] > num[j])
+			if (student[i] < student[j])
 			{
-				int temp = num[i];
-				num[i] = num[j];
-				num[j] = temp;
+				int temp = student[i];
+				student[i] = student[j];
+				student[j] = temp;
 			}
 		}
 	}
 
-	std::cout << sum/5 << std::endl;
-	std::cout << num[2] << std::endl;
+	std::cout << student[k - 1] << std::endl;
+
+	delete[] student;
 }
