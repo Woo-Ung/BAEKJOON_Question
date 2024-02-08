54575cc8171d6ec8c40a6572167718ae071fe1d2
ehddnr <ehddnrdlaos@gmail.com>
2024-02-08 23:41:01 +0900

    Update/0208 2

--- step14/no.14.cpp
+++ step14/no.14.cpp
@@ -3,64 +3,97 @@
 #include <algorithm>
 
 //1.
+//int main()
+//{
+//	std::ios::sync_with_stdio(false);
+//	std::cin.tie(nullptr);
+//	std::cout.tie(NULL);
+//
+//	int N{}, M{};
+//	std::vector<int> array{};
+//	std::vector<int> num{};
+//	std::vector<int> check{};
+//
+//	std::cin >> N;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		int x{};
+//		std::cin >> x;
+//		array.push_back(x);
+//	}
+//
+//	std::sort(array.begin(), array.end());
+//
+//	std::cin >> M;
+//
+//	for (int i = 0; i < M; i++)
+//	{
+//		int y{};
+//		std::cin >> y;
+//		num.push_back(y);
+//		check.push_back(0);
+//	}
+//
+//	for (int i = 0; i < M;i++)
+//	{
+//		int right = N - 1;
+//		int left = 0;
+//		
+//		while (left <= right)
+//		{			
+//			int mid = (right + left) / 2;
+//			if (num[i] == array[mid])
+//			{
+//				check[i] = 1;
+//				break;
+//			}
+//			if (num[i] < array[mid])
+//			{
+//				right = mid - 1;				
+//			}
+//			else
+//			{
+//				left = mid + 1;				
+//			}
+//		}
+//	}
+//
+//	for (int i = 0; i < M;i++)
+//	{
+//		std::cout << check[i] << " ";
+//	}
+//}
+
+//2.
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(NULL);
 
-	int N{}, M{};
-	std::vector<int> array{};
-	std::vector<int> num{};
-	std::vector<int> check{};
+	int N{}, M{}, count{};
+	std::string x{};
+	std::vector<std::string> check{};
 
-	std::cin >> N;
+	std::cin >> N >> M;
 
 	for (int i = 0; i < N; i++)
 	{
-		int x{};
 		std::cin >> x;
-		array.push_back(x);
+		check.push_back(x);
 	}
 
-	std::sort(array.begin(), array.end());
-
-	std::cin >> M;
+	std::sort(check.begin(), check.end());
 
 	for (int i = 0; i < M; i++)
 	{
-		int y{};
-		std::cin >> y;
-		num.push_back(y);
-		check.push_back(0);
-	}
-
-	for (int i = 0; i < M;i++)
-	{
-		int right = N - 1;
-		int left = 0;
-		
-		while (left <= right)
-		{			
-			int mid = (right + left) / 2;
-			if (num[i] == array[mid])
-			{
-				check[i] = 1;
-				break;
-			}
-			if (num[i] < array[mid])
-			{
-				right = mid - 1;				
-			}
-			else
-			{
-				left = mid + 1;				
-			}
+		std::cin >> x;
+		if (std::binary_search(check.begin(), check.end(), x))
+		{
+			count++;
 		}
 	}
 
-	for (int i = 0; i < M;i++)
-	{
-		std::cout << check[i] << " ";
-	}
+	std::cout << count << std::endl;
 }
