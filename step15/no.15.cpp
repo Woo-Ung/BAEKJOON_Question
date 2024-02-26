9f96b05c1eddfb639f933434ed9a709e71361cba
ehddnr <ehddnrdlaos@gmail.com>
2024-02-26 22:33:47 +0900

    Update/0226 2

--- step15/no.15.cpp
+++ step15/no.15.cpp
@@ -2,67 +2,104 @@
 #include <vector>
 
 //1.
+//int main()
+//{
+//	std::ios::sync_with_stdio(false);
+//	std::cin.tie(nullptr);
+//	std::cout.tie(NULL);
+//
+//	int T{}, A{}, B{}, C{}, D{};	
+//
+//	std::cin >> T;
+//
+//	for (int i = 0;i < T;i++)
+//	{
+//		std::vector<int> num{};
+//		int answer{ 1 };
+//
+//		std::cin >> A >> B;
+//
+//		if (A > B)
+//		{
+//			C = A;
+//			D = B;
+//		}
+//		else
+//		{
+//			C = B;
+//			D = A;
+//		}
+//
+//		int E = C;
+//
+//		for (int i = 2; i <= E;i++)
+//		{
+//			if (C == 1)
+//			{
+//				break;
+//			}
+//			if (C % i == 0)
+//			{
+//				C = C / i;
+//				num.push_back(i);
+//				i--;
+//			}
+//		}
+//
+//		for (int i = 0; i < size(num); i++)
+//		{
+//			if (D == 1)
+//			{
+//				break;
+//			}
+//			if (D % num[i] == 0)
+//			{
+//				D = D / num[i];
+//			}
+//		}
+//
+//		for (auto e : num)
+//		{
+//			answer *= e;
+//		}
+//
+//		std::cout << answer * D << '\n';
+//	}
+//}
+
+//2.
+int gcd(int a, int b)
+{
+	int r;
+	while (b != 0)
+	{
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 	std::cout.tie(NULL);
 
-	int T{}, A{}, B{}, C{}, D{};	
+	long long int A{}, B{}, C{}, D{};
 
-	std::cin >> T;
+	std::cin >> A >> B;
 
-	for (int i = 0;i < T;i++)
+	if (A > B)
 	{
-		std::vector<int> num{};
-		int answer{ 1 };
-
-		std::cin >> A >> B;
-
-		if (A > B)
-		{
-			C = A;
-			D = B;
-		}
-		else
-		{
-			C = B;
-			D = A;
-		}
-
-		int E = C;
-
-		for (int i = 2; i <= E;i++)
-		{
-			if (C == 1)
-			{
-				break;
-			}
-			if (C % i == 0)
-			{
-				C = C / i;
-				num.push_back(i);
-				i--;
-			}
-		}
-
-		for (int i = 0; i < size(num); i++)
-		{
-			if (D == 1)
-			{
-				break;
-			}
-			if (D % num[i] == 0)
-			{
-				D = D / num[i];
-			}
-		}
-
-		for (auto e : num)
-		{
-			answer *= e;
-		}
-
-		std::cout << answer * D << '\n';
+		C = A;
+		D = B;
 	}
+	else
+	{
+		C = B;
+		D = A;
+	}
+
+	std::cout << (C * D) / gcd(C, D) << '\n';
 }
