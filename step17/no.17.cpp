a91452b1b7f274e5163cdb3597aedd1048a71489
ehddnr <ehddnrdlaos@gmail.com>
2024-03-26 23:36:55 +0900

    Update/0326 2

--- step17/no.17.cpp
+++ step17/no.17.cpp
@@ -1,11 +1,23 @@
 #include<iostream>
+#include <cmath>
 
 //1.
+//int main()
+//{
+//	int N{};
+//
+//	std::cin >> N;
+//
+//	std::cout << N * (N - 1) << std::endl;
+//}
+
+//2.
 int main()
 {
 	int N{};
 
 	std::cin >> N;
 
-	std::cout << N * (N - 1) << std::endl;
+	std::cout << pow(2, N) << std::endl;
+
 }
