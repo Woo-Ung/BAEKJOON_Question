b346c8cce29c24580aa220a3925bf6f865ea49e1
ehddnr <ehddnrdlaos@gmail.com>
2024-03-31 23:32:43 +0900

    Update/0331 3

--- step17/no.17.cpp
+++ step17/no.17.cpp
@@ -12,12 +12,34 @@
 //}
 
 //2.
+//int main()
+//{
+//	int N{};
+//
+//	std::cin >> N;
+//
+//	std::cout << pow(2, N) << std::endl;
+//
+//}
+
+//3.
 int main()
 {
-	int N{};
+	int N{}, P{1};
 
 	std::cin >> N;
 
-	std::cout << pow(2, N) << std::endl;
+	if (N == 0)
+	{
+		std::cout << 1 << std::endl;
+	}
 
+	else
+	{
+		for (int i = N; i > 0;i--)
+		{
+			P = P * i;
+		}
+		std::cout << P << std::endl;
+	}
 }
