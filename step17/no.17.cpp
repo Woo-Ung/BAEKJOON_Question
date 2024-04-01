d7e629227446345b267e16812a97063485a5e02a
ehddnr <ehddnrdlaos@gmail.com>
2024-04-01 23:35:02 +0900

    Update/0401 4

--- step17/no.17.cpp
+++ step17/no.17.cpp
@@ -23,23 +23,53 @@
 //}
 
 //3.
-int main()
+//int main()
+//{
+//	int N{}, P{1};
+//
+//	std::cin >> N;
+//
+//	if (N == 0)
+//	{
+//		std::cout << 1 << std::endl;
+//	}
+//
+//	else
+//	{
+//		for (int i = N; i > 0;i--)
+//		{
+//			P = P * i;
+//		}
+//		std::cout << P << std::endl;
+//	}
+//}
+
+//4.
+int factorial(int n)
 {
-	int N{}, P{1};
-
-	std::cin >> N;
-
-	if (N == 0)
+	if (n <= 1)
 	{
-		std::cout << 1 << std::endl;
+		return 1;
 	}
 
 	else
 	{
-		for (int i = N; i > 0;i--)
-		{
-			P = P * i;
-		}
-		std::cout << P << std::endl;
+		return n * factorial(n - 1);
+	}
+}
+
+int main()
+{
+	int N{}, K{};
+
+	std::cin >> N >> K;
+
+	if (K == 0 && K == N)
+	{
+		std::cout << 1 << '\n';
+	}
+	else
+	{
+		std::cout << factorial(N) / (factorial(N - K) * factorial(K)) << '\n';
 	}
 }
