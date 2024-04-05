112b07c80d840c79c274892bd8b36bccc6b0b914
ehddnr <ehddnrdlaos@gmail.com>
2024-04-05 22:30:54 +0900

    Update/0405 2

--- step18/no.18.cpp
+++ step18/no.18.cpp
@@ -1,23 +1,70 @@
 #include<iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 //1.
+//int main()
+//{
+//	int T{}, x{};
+//	std::vector<int> num;
+//
+//	std::cin >> T;
+//
+//	for (int i = 0; i < T; i++)
+//	{
+//		std::cin >> x;
+//
+//		num.push_back(x);
+//	}
+//
+//	std::sort(num.begin(), num.end());
+//
+//	std::cout << num[0] * num[size(num)-1] << '\n';
+//}
+
+//2.
 int main()
 {
-	int T{}, x{};
-	std::vector<int> num;
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
 
-	std::cin >> T;
+	int N{}, count{};
+	std::string x{};
+	std::map<std::string, int> name{};
+	
 
-	for (int i = 0; i < T; i++)
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++)
 	{
 		std::cin >> x;
 
-		num.push_back(x);
+		if (x == "ENTER")
+		{
+			for (auto& e : name)
+			{
+				if (e.second > 0)
+				{
+					count++;					
+				}			
+			}
+			name.clear();
+		}
+		else
+		{
+			name[x]++;
+		}
 	}
 
-	std::sort(num.begin(), num.end());
+	for (auto& e : name)
+	{
+		if (e.second > 0)
+		{
+			count++;
+		}
+	}
 
-	std::cout << num[0] * num[size(num)-1] << '\n';
+	std::cout << count << '\n';
 }
