7548426734024ad84b819c13a77b5220d6dfd4b6
ehddnr <ehddnrdlaos@gmail.com>
2024-04-09 00:06:18 +0900

    Update/0409 5

--- step18/no.18.cpp
+++ step18/no.18.cpp
@@ -105,79 +105,122 @@
 //}
 
 //4.
-bool cmp(std::pair<int, int>& a, std::pair<int, int>& b)
+//bool cmp(std::pair<int, int>& a, std::pair<int, int>& b)
+//{
+//	if (a.second == b.second)
+//	{
+//		return a.first > b.first;
+//	}
+//
+//	else
+//	{
+//		return a.second < b.second;
+//	}
+//}
+//
+//int main()
+//{
+//	double max{}, x{}, N{}, mid{}, most{}, range{};
+//
+//	std::vector<int> array;
+//	std::map<int, int> array2;
+//
+//	std::cin >> N;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		std::cin >> x;
+//
+//		max = max + x;
+//
+//		array.push_back(x);
+//		array2[x]++;
+//	}
+//
+//	std::vector<std::pair<int, int>> vec(array2.begin(), array2.end());
+//
+//	std::sort(array.begin(), array.end());
+//
+//	std::sort(vec.begin(), vec.end(),cmp);
+//
+//	range = array[size(array)-1] - array[0];
+//
+//	if (size(vec) > 1)
+//	{
+//		if (vec[vec.size() - 1].second == vec[vec.size() - 2].second)
+//		{
+//			most = vec[vec.size() - 2].first;
+//		}
+//		else
+//		{
+//			most = vec[vec.size() - 1].first;
+//		}
+//	}
+//	else
+//	{
+//		most = vec[vec.size() - 1].first;
+//	}
+//		
+//	if (max < 0)
+//	{
+//		if (round((abs(max) / N)) * -1 == -0)
+//		{
+//			std::cout << 0 << '\n';
+//		}
+//		else
+//		{
+//			std::cout << round((abs(max) / N)) * -1 << '\n';
+//		}
+//	}
+//	else
+//	{
+//		std::cout << round(max / N) << '\n';
+//	}
+//
+//	std::cout << array[N/2] << '\n';
+//	std::cout << most << '\n';
+//	std::cout << range << '\n';
+//}
+
+//5.
+bool cmp(std::pair<std::string, int>& a, std::pair<std::string, int>& b)
 {
 	if (a.second == b.second)
 	{
-		return a.first > b.first;
+		if (size(a.first) == size(b.first))
+		{
+			return a.first < b.first;			
+		}
+		return size(a.first) > size(b.first);		
 	}
+	return a.second > b.second;	
+};
 
-	else
-	{
-		return a.second < b.second;
-	}
-}
 
 int main()
 {
-	double max{}, x{}, N{}, mid{}, most{}, range{};
+	int N{}, M{};
 
-	std::vector<int> array;
-	std::map<int, int> array2;
+	std::string x;
+	std::map<std::string, int> array;
 
-	std::cin >> N;
+	std::cin >> N >> M;
 
 	for (int i = 0; i < N; i++)
 	{
 		std::cin >> x;
-
-		max = max + x;
-
-		array.push_back(x);
-		array2[x]++;
-	}
-
-	std::vector<std::pair<int, int>> vec(array2.begin(), array2.end());
-
-	std::sort(array.begin(), array.end());
-
-	std::sort(vec.begin(), vec.end(),cmp);
-
-	range = array[size(array)-1] - array[0];
-
-	if (size(vec) > 1)
-	{
-		if (vec[vec.size() - 1].second == vec[vec.size() - 2].second)
-		{
-			most = vec[vec.size() - 2].first;
-		}
-		else
-		{
-			most = vec[vec.size() - 1].first;
-		}
-	}
-	else
-	{
-		most = vec[vec.size() - 1].first;
-	}
 		
-	if (max < 0)
-	{
-		if (round((abs(max) / N)) * -1 == -0)
+		if (size(x) >= M)
 		{
-			std::cout << 0 << '\n';
+			array[x]++;
 		}
-		else
-		{
-			std::cout << round((abs(max) / N)) * -1 << '\n';
-		}
-	}
-	else
-	{
-		std::cout << round(max / N) << '\n';
 	}
 
-	std::cout << array[N/2] << '\n';
-	std::cout << most << '\n';
-	std::cout << range << '\n';
+	std::vector<std::pair<std::string, int>> vec(array.begin(),array.end());
+	std::sort(vec.begin(), vec.end(), cmp);
+
+	for (auto& e : vec)
+	{
+		std::cout << e.first << '\n';
+	}
 }
