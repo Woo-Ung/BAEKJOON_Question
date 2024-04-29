e124e59f3c08b2755a280307b9d0ea0ecef9d17c
ehddnr <ehddnrdlaos@gmail.com>
2024-04-29 23:29:27 +0900

    Update/0429 3

--- step20/no.20.cpp
+++ step20/no.20.cpp
@@ -37,11 +37,44 @@
 
 //2.
 
+//int N{}, M{};
+//int array[9];
+//bool check[9];
+//
+//void dfs(int b, int a)
+//{
+//	if (a == M)
+//	{
+//		for (int i = 0; i < M;i++)
+//		{
+//			std::cout << array[i] << " ";
+//		}std::cout << '\n';
+//	}
+//
+//	for (auto i = b; i <= N; i++)
+//	{
+//		if (!check[i])
+//		{
+//			check[i] = true;
+//			array[a] = i;
+//			dfs(i + 1, a + 1);
+//			check[i] = false;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	std::cin >> N >> M;
+//	dfs(1,0);
+//}
+
+//3.
 int N{}, M{};
 int array[9];
 bool check[9];
 
-void dfs(int b, int a)
+void dfs(int a)
 {
 	if (a == M)
 	{
@@ -49,22 +82,20 @@ void dfs(int b, int a)
 		{
 			std::cout << array[i] << " ";
 		}std::cout << '\n';
+		return;
 	}
 
-	for (auto i = b; i <= N; i++)
+	for (auto i = 1; i <= N; i++)
 	{
-		if (!check[i])
-		{
-			check[i] = true;
-			array[a] = i;
-			dfs(i + 1, a + 1);
-			check[i] = false;
-		}
+		check[i] = true;
+		array[a] = i;
+		dfs(a + 1);
+		check[i] = false;		
 	}
 }
 
 int main()
 {
 	std::cin >> N >> M;
-	dfs(1,0);
+	dfs(0);
 }
