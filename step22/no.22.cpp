719a9210eb9f640d6e94adc6a38b68afafbdcba5
ehddnr <ehddnrdlaos@gmail.com>
2024-06-15 22:11:33 +0900

    Update/0615 2

--- step22/no.22.cpp
+++ step22/no.22.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <climits>
 
 //1.
+//
+//int main()
+//{
+//	std::ios::sync_with_stdio(false);
+//	std::cin.tie(NULL);
+//	std::cout.tie(NULL);
+//
+//	int N{}, M{}, i{}, j{};
+//
+//	std::vector<long long int> arry{ 0, };
+//
+//	std::cin >> N >> M;
+//
+//	for (int a = 1; a <= N;a++)
+//	{
+//		long long int x{};
+//		std::cin >> x;
+//		x += arry[a - 1];	
+//
+//		arry.push_back(x);
+//	}
+//
+//	for (int a = 0; a < M;a++)
+//	{		
+//		std::cin >> i >> j;		
+//		long long int sum{};
+//
+//		sum = arry[j] - arry[i - 1];
+//
+//		std::cout << sum << '\n';
+//	}
+//
+//}
+
+//2.
 
 int main()
 {
@@ -9,29 +46,24 @@ int main()
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 
-	int N{}, M{}, i{}, j{};
+	int N{}, K{}, mx{INT_MIN};
 
-	std::vector<long long int> arry{ 0, };
+	int arry[100001]{};
 
-	std::cin >> N >> M;
+	std::cin >> N >> K;
 
-	for (int a = 1; a <= N;a++)
+	for (int i = 1; i <= N;i++)
 	{
-		long long int x{};
-		std::cin >> x;
-		x += arry[a - 1];	
+		int x{};
+		std::cin >> x;		
 
-		arry.push_back(x);
+		arry[i] = arry[i - 1] + x;
 	}
 
-	for (int a = 0; a < M;a++)
-	{		
-		std::cin >> i >> j;		
-		long long int sum{};
-
-		sum = arry[j] - arry[i - 1];
-
-		std::cout << sum << '\n';
+	for (int i = K; i <= N; i++)
+	{
+		mx = std::max(mx, arry[i] - arry[i-K]);
 	}
 
+	std::cout << mx << '\n';
 }
