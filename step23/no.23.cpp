1d16e0c4d0d62d401e9edd7efe521bec9a2440c8
ehddnr <ehddnrdlaos@gmail.com>
2024-06-24 23:42:29 +0900

    Update/0624 2

--- step23/no.23.cpp
+++ step23/no.23.cpp
@@ -4,35 +4,79 @@
 
 //1.
 
+//int main()
+//{
+//	int N{}, K{}, count{};
+//	std::vector<int> coin;
+//
+//	std::cin >> N >> K;
+//
+//	for (int i = 0; i < N;i++)
+//	{
+//		int x{};
+//		std::cin >> x;
+//		coin.push_back(x);
+//	}
+//
+//	std::sort(coin.begin(), coin.end(),std::greater<int>());
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		if (K == 0)
+//		{
+//			break;
+//		}
+//		if (K >= coin[i])
+//		{
+//			K -= coin[i];
+//			count++;
+//			i--;
+//		}
+//	}
+//	
+//	std::cout << count << '\n';
+//}
+
+//2.
+bool cmp(std::pair<int, int> a, std::pair<int, int> b)
+{
+	if (a.second == b.second)
+	{
+		return a.first < b.first;
+	}
+
+	return a.second < b.second;
+}
+
 int main()
 {
-	int N{}, K{}, count{};
-	std::vector<int> coin;
+	int N{}, count{1};
 
-	std::cin >> N >> K;
+	std::vector<std::pair<int, int>> time;
+
+	std::cin >> N;
 
 	for (int i = 0; i < N;i++)
 	{
-		int x{};
-		std::cin >> x;
-		coin.push_back(x);
+		int x{}, y{};
+		std::cin >> x >> y;
+
+		time.push_back({ x,y });
 	}
 
-	std::sort(coin.begin(), coin.end(),std::greater<int>());
+	std::sort(time.begin(), time.end(),cmp);
 
-	for (int i = 0; i < N; i++)
-	{
-		if (K == 0)
+	int start = time[0].second;
+
+	for (int i = 1; i < N;i++)
+	{			
+		if (time[i].first >= start)
 		{
-			break;
-		}
-		if (K >= coin[i])
-		{
-			K -= coin[i];
 			count++;
-			i--;
+
+			start = time[i].second;
 		}
 	}
-	
+
 	std::cout << count << '\n';
 }
