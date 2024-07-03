dbc8c1179729018491fff0b07655748f9f5b9288
ehddnr <ehddnrdlaos@gmail.com>
2024-07-03 23:11:03 +0900

    Update/0703 3

--- step24/no.24.cpp
+++ step24/no.24.cpp
@@ -66,40 +66,110 @@
 
 //2.
 
-char ret[64][64] {};
+//char ret[64][64] {};
+//
+//void check(int a, int b, int c)
+//{
+//	bool cut{ false };
+//
+//	char point = ret[a][b];
+//
+//	for (int i = a; i < a + c;i++)
+//	{
+//		for (int j = b; j < b + c;j++)
+//		{
+//			if (point != ret[i][j])
+//			{
+//				cut = true;
+//				break;
+//			}			
+//		}
+//
+//	}	
+//
+//	if (cut)
+//	{
+//		std::cout << "(";
+//		check(a, b, c / 2);
+//		check(a, b + c / 2, c / 2);
+//		check(a + c / 2, b, c / 2);
+//		check(a + c / 2, b + c / 2, c / 2);
+//		std::cout << ")";
+//	}
+//
+//	else
+//	{
+//		std::cout << point;		
+//	}
+//}
+//
+//int main()
+//{
+//	int N{};
+//
+//	std::cin >> N;
+//
+//	for (int i = 0; i < N;i++)
+//	{
+//		for (int j = 0; j < N;j++)
+//		{
+//			std::cin >> ret[i][j];
+//		}
+//	}
+//
+//	check(0, 0, N);
+//}
+
+//3.
+
+int one{}, zero{}, mone{};
+int ret[2187][2187];
 
 void check(int a, int b, int c)
 {
 	bool cut{ false };
 
-	char point = ret[a][b];
+	int point = ret[a][b];
 
-	for (int i = a; i < a + c;i++)
+	for (int i = a; i < a + c; i++)
 	{
-		for (int j = b; j < b + c;j++)
+		for (int j = b; j < b + c; j++)
 		{
-			if (point != ret[i][j])
+			if (ret[i][j] != point)
 			{
 				cut = true;
 				break;
-			}			
+			}
 		}
-
-	}	
+	}
 
 	if (cut)
 	{
-		std::cout << "(";
-		check(a, b, c / 2);
-		check(a, b + c / 2, c / 2);
-		check(a + c / 2, b, c / 2);
-		check(a + c / 2, b + c / 2, c / 2);
-		std::cout << ")";
+		check(a, b, c / 3);
+		check(a + c / 3, b, c / 3);
+		check(a + c * 2 / 3, b, c / 3);
+		check(a, b + c / 3, c / 3);
+		check(a + c / 3, b + c / 3, c / 3);
+		check(a + c * 2/ 3, b + c / 3, c / 3);
+		check(a, b + c * 2 / 3, c / 3);
+		check(a + c / 3, b + c * 2 / 3, c / 3);
+		check(a + c * 2 / 3, b + c * 2 / 3, c / 3);
 	}
 
 	else
 	{
-		std::cout << point;		
+		if (point == 1)
+		{
+			one++;
+		}
+		else if (point == 0)
+		{
+			zero++;
+		}
+		else
+		{
+			mone++;
+		}
 	}
 }
 
@@ -109,13 +179,16 @@ int main()
 
 	std::cin >> N;
 
-	for (int i = 0; i < N;i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < N;j++)
+		for (int j = 0; j < N; j++)
 		{
 			std::cin >> ret[i][j];
 		}
 	}
 
 	check(0, 0, N);
+
+	std::cout << mone << '\n' << zero << '\n' << one << '\n';
+
 }
