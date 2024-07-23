681de79d6a30b9187721db0c7cf9513cc970d9d9
ehddnr <ehddnrdlaos@gmail.com>
2024-07-23 23:00:57 +0900

    Update/0723 2

--- step26/no.26.cpp
+++ step26/no.26.cpp
@@ -3,22 +3,62 @@
 
 //1.
 
+//int main()
+//{
+//	std::ios::sync_with_stdio(false);
+//	std::cout.tie(NULL);
+//	std::cin.tie(NULL);
+//
+//	int N{};
+//	std::priority_queue<int> arry;
+//
+//	std::cin >> N;
+//
+//	for (int i = 0; i < N;i++)
+//	{
+//		int x{};
+//
+//		std::cin >> x;
+//		if (x == 0)
+//		{
+//			if (arry.empty())
+//			{
+//				std::cout << 0 << '\n';
+//			}
+//			else
+//			{
+//				std::cout << arry.top() << '\n';
+//				arry.pop();
+//			}
+//		}
+//
+//		else
+//		{
+//			arry.push(x);
+//		}
+//	}
+//}
+
+//2.
+
 int main()
 {
 	std::ios::sync_with_stdio(false);
-	std::cout.tie(NULL);
 	std::cin.tie(NULL);
+	std::cout.tie(NULL);
 
 	int N{};
-	std::priority_queue<int> arry;
+
+	std::priority_queue<int,std::vector<int>,std::greater<int>> arry;
 
 	std::cin >> N;
 
-	for (int i = 0; i < N;i++)
+	for (int i = 0; i < N; i++)
 	{
 		int x{};
 
 		std::cin >> x;
+
 		if (x == 0)
 		{
 			if (arry.empty())
