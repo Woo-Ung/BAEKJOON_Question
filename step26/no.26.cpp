e0bdb00d48b24728b6cb96385fc8525523c8145a
ehddnr <ehddnrdlaos@gmail.com>
2024-07-24 22:03:53 +0900

    Update/0724 3

--- step26/no.26.cpp
+++ step26/no.26.cpp
@@ -41,6 +41,59 @@
 
 //2.
 
+//int main()
+//{
+//	std::ios::sync_with_stdio(false);
+//	std::cin.tie(NULL);
+//	std::cout.tie(NULL);
+//
+//	int N{};
+//
+//	std::priority_queue<int,std::vector<int>,std::greater<int>> arry;
+//
+//	std::cin >> N;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		int x{};
+//
+//		std::cin >> x;
+//
+//		if (x == 0)
+//		{
+//			if (arry.empty())
+//			{
+//				std::cout << 0 << '\n';
+//			}
+//			else
+//			{
+//				std::cout << arry.top() << '\n';
+//				arry.pop();
+//			}
+//		}
+//
+//		else
+//		{
+//			arry.push(x);
+//		}
+//	}
+//}
+
+//3.
+
+struct cmp
+{
+	bool operator()(int a, int b)
+	{
+		if (abs(a) == abs(b))
+		{
+			return a > b;
+		}
+
+		return abs(a) > abs(b);
+	}
+};
+
 int main()
 {
 	std::ios::sync_with_stdio(false);
@@ -49,7 +102,7 @@ int main()
 
 	int N{};
 
-	std::priority_queue<int,std::vector<int>,std::greater<int>> arry;
+	std::priority_queue<int, std::vector<int>, cmp> arry;
 
 	std::cin >> N;
 
