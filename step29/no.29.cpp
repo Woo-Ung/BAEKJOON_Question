a77bdfcf9cc3867c014a9804b8c9e691a0dc3b32
ehddnr <ehddnrdlaos@gmail.com>
2024-08-18 23:08:43 +0900

    Update/0818 2

--- step29/no.29.cpp
+++ step29/no.29.cpp
@@ -4,7 +4,61 @@
 
 //1.
 
-int visited[100001]{ 0, };
+//int visited[100001]{ 0, };
+//int result[100001]{};
+//int count{};
+//
+//std::vector<int> graph[100001];
+//
+//void dfs(int a)
+//{
+//	if (visited[a] == 1)
+//	{
+//		return;
+//	}
+//
+//	visited[a] = 1;
+//	count++;
+//	result[a] = count;
+//
+//	for (int i = 0; i < graph[a].size();i++)
+//	{
+//		dfs(graph[a][i]);
+//	}
+//
+//}
+//
+//int main()
+//{
+//	int N{}, M{}, R{};
+//
+//	std::cin >> N >> M >> R;
+//
+//	for (int i = 1; i <= M;i++)
+//	{
+//		int x{}, y{};
+//
+//		std::cin >> x >> y;
+//		graph[x].push_back(y);
+//		graph[y].push_back(x);
+//	}
+//	
+//	for (int i = 1;i <= N;i++)
+//	{
+//		std::sort(graph[i].begin(), graph[i].end());
+//	}
+//
+//	dfs(R);
+//
+//	for (int i = 1;i <= N;i++)
+//	{
+//		std::cout << result[i] << '\n';
+//	}
+//}
+
+//2.
+
+int visited[100001]{0,};
 int result[100001]{};
 int count{};
 
@@ -25,7 +79,6 @@ void dfs(int a)
 	{
 		dfs(graph[a][i]);
 	}
-
 }
 
 int main()
@@ -34,18 +87,19 @@ int main()
 
 	std::cin >> N >> M >> R;
 
-	for (int i = 1; i <= M;i++)
+	for (int i = 1;i <= M;i++)
 	{
 		int x{}, y{};
 
 		std::cin >> x >> y;
+
 		graph[x].push_back(y);
 		graph[y].push_back(x);
 	}
-	
+
 	for (int i = 1;i <= N;i++)
 	{
-		std::sort(graph[i].begin(), graph[i].end());
+		std::sort(graph[i].begin(), graph[i].end(), std::greater<int>());
 	}
 
 	dfs(R);
