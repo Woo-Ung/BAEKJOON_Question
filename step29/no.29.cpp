589faf6229c0a1f41d4fd882e2c16abf0092435f
ehddnr <ehddnrdlaos@gmail.com>
2024-08-19 23:50:25 +0900

    Update/0819 3

--- step29/no.29.cpp
+++ step29/no.29.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 //1.
 
@@ -58,26 +59,88 @@
 
 //2.
 
-int visited[100001]{0,};
+//int visited[100001]{0,};
+//int result[100001]{};
+//int count{};
+//
+//std::vector<int> graph[100001];
+//
+//void dfs(int a)
+//{
+//	if (visited[a] == 1)
+//	{
+//		return;
+//	}
+//
+//	visited[a] = 1;
+//	count++;
+//	result[a] = count;
+//
+//	for (int i = 0; i < graph[a].size();i++)
+//	{
+//		dfs(graph[a][i]);
+//	}
+//}
+//
+//int main()
+//{
+//	int N{}, M{}, R{};
+//
+//	std::cin >> N >> M >> R;
+//
+//	for (int i = 1;i <= M;i++)
+//	{
+//		int x{}, y{};
+//
+//		std::cin >> x >> y;
+//
+//		graph[x].push_back(y);
+//		graph[y].push_back(x);
+//	}
+//
+//	for (int i = 1;i <= N;i++)
+//	{
+//		std::sort(graph[i].begin(), graph[i].end(), std::greater<int>());
+//	}
+//
+//	dfs(R);
+//
+//	for (int i = 1;i <= N;i++)
+//	{
+//		std::cout << result[i] << '\n';
+//	}
+//}
+
+//3.
+
+int visited[100001]{ 0, };
 int result[100001]{};
 int count{};
 
 std::vector<int> graph[100001];
+std::queue<int> q;
 
-void dfs(int a)
+void bfs(int a)
 {
-	if (visited[a] == 1)
-	{
-		return;
-	}
+	q.push(a);
 
-	visited[a] = 1;
-	count++;
-	result[a] = count;
-
-	for (int i = 0; i < graph[a].size();i++)
+	while (!q.empty())
 	{
-		dfs(graph[a][i]);
+		int b = q.front();
+		q.pop();
+		
+		visited[b] = 1;
+		count++;
+		result[b] = count;
+
+		for (int i = 0;i < graph[b].size();i++)
+		{
+			if (!visited[graph[b][i]])
+			{	
+				visited[graph[b][i]] = 1;
+				q.push(graph[b][i]);
+			}
+		}
 	}
 }
 
@@ -99,10 +162,10 @@ int main()
 
 	for (int i = 1;i <= N;i++)
 	{
-		std::sort(graph[i].begin(), graph[i].end(), std::greater<int>());
+		std::sort(graph[i].begin(), graph[i].end());
 	}
 
-	dfs(R);
+	bfs(R);
 
 	for (int i = 1;i <= N;i++)
 	{
