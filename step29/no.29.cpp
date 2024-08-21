10eea8b3a2c6fac3fe31ecb645350f3a46eaeb78
ehddnr <ehddnrdlaos@gmail.com>
2024-08-21 22:20:46 +0900

    Update/0821 4

--- step29/no.29.cpp
+++ step29/no.29.cpp
@@ -113,6 +113,68 @@
 
 //3.
 
+//int visited[100001]{ 0, };
+//int result[100001]{};
+//int count{};
+//
+//std::vector<int> graph[100001];
+//std::queue<int> q;
+//
+//void bfs(int a)
+//{
+//	q.push(a);
+//
+//	while (!q.empty())
+//	{
+//		int b = q.front();
+//		q.pop();
+//		
+//		visited[b] = 1;
+//		count++;
+//		result[b] = count;
+//
+//		for (int i = 0;i < graph[b].size();i++)
+//		{
+//			if (!visited[graph[b][i]])
+//			{	
+//				visited[graph[b][i]] = 1;
+//				q.push(graph[b][i]);
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int N{}, M{}, R{};
+//
+//	std::cin >> N >> M >> R;
+//
+//	for (int i = 1;i <= M;i++)
+//	{
+//		int x{}, y{};
+//
+//		std::cin >> x >> y;
+//
+//		graph[x].push_back(y);
+//		graph[y].push_back(x);
+//	}
+//
+//	for (int i = 1;i <= N;i++)
+//	{
+//		std::sort(graph[i].begin(), graph[i].end());
+//	}
+//
+//	bfs(R);
+//
+//	for (int i = 1;i <= N;i++)
+//	{
+//		std::cout << result[i] << '\n';
+//	}
+//}
+
+//4.
+
 int visited[100001]{ 0, };
 int result[100001]{};
 int count{};
@@ -127,16 +189,17 @@ void bfs(int a)
 	while (!q.empty())
 	{
 		int b = q.front();
+
 		q.pop();
-		
+
 		visited[b] = 1;
 		count++;
 		result[b] = count;
 
-		for (int i = 0;i < graph[b].size();i++)
+		for (int i = 0; i < graph[b].size();i++)
 		{
 			if (!visited[graph[b][i]])
-			{	
+			{
 				visited[graph[b][i]] = 1;
 				q.push(graph[b][i]);
 			}
@@ -162,7 +225,7 @@ int main()
 
 	for (int i = 1;i <= N;i++)
 	{
-		std::sort(graph[i].begin(), graph[i].end());
+		std::sort(graph[i].begin(), graph[i].end(), std::greater<int>());
 	}
 
 	bfs(R);
