86584e4386fa4d7169876241ce045028881033bf
ehddnr <ehddnrdlaos@gmail.com>
2024-10-01 23:33:38 +0900

    Update/1001 2

--- step31/no.31.cpp
+++ step31/no.31.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
 #include <algorithm>
 
+
 //1.
 
-int N{}, x{}, ans{};
-int arr[1000001]{};
+//int N{}, x{}, ans{};
+//int arr[1000001]{};
+//
+//int main()
+//{
+//	std::cin >> N;
+//
+//	for (int i = 0; i < N;i++)
+//	{
+//		std::cin >> arr[i];
+//	}
+//
+//	std::cin >> x;
+//
+//	std::sort(arr, arr+N);
+//
+//	int l = 0;
+//	int r = N - 1;
+//
+//	while (l < r)
+//	{
+//		int sum = arr[l] + arr[r];
+//
+//		if (sum < x)
+//		{
+//			l++;
+//		}
+//		else if (sum > x)
+//		{
+//			r--;
+//		}
+//
+//		else if (sum == x)
+//		{
+//			ans++;
+//			l++;
+//			r--;
+//		}
+//	}
+//
+//	std::cout << ans << '\n';
+//}
+
+
+//2.
+
+int N{}, min{ 2000000001 }, a{}, b{};
+int arr[100001]{};
 
 int main()
 {
@@ -15,33 +62,37 @@ int main()
 		std::cin >> arr[i];
 	}
 
-	std::cin >> x;
-
-	std::sort(arr, arr+N);
+	std::sort(arr, arr + N);
 
 	int l = 0;
 	int r = N - 1;
 
-	while (l < r)
+	while (l<r)
 	{
 		int sum = arr[l] + arr[r];
 
-		if (sum < x)
+		if (min > abs(sum))
 		{
-			l++;
-		}
-		else if (sum > x)
-		{
-			r--;
+			min = abs(sum);
+			a = arr[l];
+			b = arr[r];
+
+			if (sum == 0)
+			{
+				break;
+			}
 		}
 
-		else if (sum == x)
+		if (sum < 0)
 		{
-			ans++;
 			l++;
+		}
+
+		else
+		{
 			r--;
 		}
 	}
 
-	std::cout << ans << '\n';
+	std::cout << a << " " << b << '\n';
 }
