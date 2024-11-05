b0ccde48870641d4aa2b6d9f97e13cc0b647a5aa
ehddnr <ehddnrdlaos@gmail.com>
2024-11-06 00:00:03 +0900

    Update/1105 2

--- step33/no.33.cpp
+++ step33/no.33.cpp
@@ -2,72 +2,157 @@
 
 //1.
 
-int n{}, m{};
-int parent[1000001];
+//int n{}, m{};
+//int parent[1000001];
+//
+//int getP(int x)
+//{
+//	if (parent[x] == x)
+//	{
+//		return x;
+//	}
+//
+//	return parent[x] = getP(parent[x]);
+//}
+//
+//void unionP(int x, int y)
+//{
+//	x = getP(x);
+//	y = getP(y);
+//
+//	if (x > y) parent[x] = y;
+//	else
+//	{
+//		parent[y] = x;
+//	}
+//}
+//
+//void findP(int x, int y)
+//{
+//	x = getP(x);
+//	y = getP(y);
+//
+//	if (x == y)
+//	{
+//		std::cout << "YES" << '\n';
+//	}
+//	else
+//	{
+//		std::cout << "NO" << '\n';
+//	}
+//}
+//
+//int main()
+//{
+//	std::ios::sync_with_stdio(false);
+//	std::cout.tie(NULL);
+//	std::cin.tie(NULL);
+//
+//	std::cin >> n >> m;
+//
+//	for (int i = 1;i <= n;i++)
+//	{
+//		parent[i] = i;
+//	}
+//
+//	for (int i = 0;i < m;i++)
+//	{
+//		int a{}, b{}, c{};
+//
+//		std::cin >> a >> b >> c;
+//
+//		if (!a)
+//		{
+//			unionP(b, c);
+//		}
+//		else if (a)
+//		{
+//			findP(b, c);
+//		}
+//	}
+//}
 
-int getP(int x)
+//2.
+
+int N{}, M{};
+
+int parent[201]{};
+int arr[1001]{};
+
+bool can{ true };
+
+int getP(int a)
 {
-	if (parent[x] == x)
+	if (parent[a] == a)
 	{
-		return x;
+		return a;
 	}
-
-	return parent[x] = getP(parent[x]);
+	return parent[a] = getP(parent[a]);
 }
 
-void unionP(int x, int y)
+void unionP(int a, int b)
 {
-	x = getP(x);
-	y = getP(y);
-
-	if (x > y) parent[x] = y;
+	a = getP(a);
+	b = getP(b);
+	if (a > b)
+	{
+		parent[a] = b;
+	}
 	else
 	{
-		parent[y] = x;
+		parent[b] = a;
 	}
 }
 
-void findP(int x, int y)
+void findP(int a, int b)
 {
-	x = getP(x);
-	y = getP(y);
+	a = getP(a);
+	b = getP(b);
+	if (a != b)
+	{
+		can = false;
+	}
+}
 
-	if (x == y)
+int main()
+{
+	std::cin >> N >> M;
+
+	for (int i = 1; i <= N;i++)
+	{
+		parent[i] = i;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= N;j++)
+		{
+			int x{};
+
+			std::cin >> x;
+			if (x)
+			{
+				unionP(i, j);
+			}
+		}
+	}
+
+	for (int i = 0; i < M;i++)
+	{
+		std::cin >> arr[i];
+	}
+
+	for (int i = 0; i < M - 1;i++)
+	{
+		findP(arr[i], arr[i + 1]);
+	}
+
+	if (can)
 	{
 		std::cout << "YES" << '\n';
 	}
 	else
 	{
 		std::cout << "NO" << '\n';
-	}
-}
-
-int main()
-{
-	std::ios::sync_with_stdio(false);
-	std::cout.tie(NULL);
-	std::cin.tie(NULL);
-
-	std::cin >> n >> m;
-
-	for (int i = 1;i <= n;i++)
-	{
-		parent[i] = i;
-	}
-
-	for (int i = 0;i < m;i++)
-	{
-		int a{}, b{}, c{};
-
-		std::cin >> a >> b >> c;
-
-		if (!a)
-		{
-			unionP(b, c);
-		}
-		else if (a)
-		{
-			findP(b, c);
-		}
 	}
 }
